45d350ff2c87553d186e1d2d836a3605700cae50
Denov <denov@Denovs-MacBook-Pro.local>
2019-04-11 14:44:10 +0300

    adding code

--- provision.sh
+++ provision.sh
@@ -18,12 +18,17 @@ sudo apt-get install unzip socat jq dnsutils net-tools vim curl sshpass nginx -y
 
 
 # install consul
+mkdir -p /tmp/pkg/
 if [ -z "$CONSUL_VER" ]; then
     CONSUL_VER=$(curl -sL https://releases.hashicorp.com/consul/index.json | jq -r '.versions[].version' | sort -V | egrep -v 'ent|beta|rc|alpha' | tail -n1)
 fi
+response=$(curl -LI https://releases.hashicorp.com/consul/${CONSUL_VER}/consul_${CONSUL_VER}_linux_amd64.zip -o /tmp/pkg/consul_${CONSUL_VER}_linux_amd64.zip -w '%{http_code}\n' -s)
 
-mkdir -p /tmp/pkg/
-curl -s https://releases.hashicorp.com/consul/${CONSUL_VER}/consul_${CONSUL_VER}_linux_amd64.zip -o /tmp/pkg/consul_${CONSUL_VER}_linux_amd64.zip
+if [ $response == 200 ]; then
+    curl -s https://releases.hashicorp.com/consul/${CONSUL_VER}/consul_${CONSUL_VER}_linux_amd64.zip -o /tmp/pkg/consul_${CONSUL_VER}_linux_amd64.zip
+else
+   exit 1
+fi
 echo "Installing Consul version ${CONSUL_VER} ..."
 pushd /tmp
 unzip /tmp/pkg/consul_${CONSUL_VER}_linux_amd64.zip 
